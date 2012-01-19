63c3ec0ea7ea2f7316502cb510fdac7b87bfce9b
jereanon <jeremy@possiblyfaulty.com>
2012-01-18 19:30:40 -0600

    sample project setup

---
+++ samples/src/main/java/com.elasticsearch.omg.samples/ExampleBusinessRunner.java
@@ -0,0 +1,11 @@
+package com.elasticsearch.omg.samples;
+
+/**
+ * Class for running examples.
+ */
+public class ExampleBusinessRunner {
+
+    public void doBusiness() {
+        // do stuffs
+    }
+}
---
+++ samples/src/main/java/com.elasticsearch.omg.samples/Tweet.java
@@ -0,0 +1,37 @@
+package com.elasticsearch.omg.samples;
+
+import java.util.Date;
+
+/**
+ * an individual tweet!
+ */
+public class Tweet {
+
+    private String user;
+    private Date postDate;
+    private String message;
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getPostDate() {
+        return postDate;
+    }
+
+    public void setPostDate(Date postDate) {
+        this.postDate = postDate;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
